a30383c298e797de459b0a1c5907f711695c8b42
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-21 14:36:21 +0300

    Adding a simple UI

--- app/src/main/java/ws/tilda/anastasia/plantplacesapp/plantplaces/SearchPlantsActivity.java
+++ app/src/main/java/ws/tilda/anastasia/plantplacesapp/plantplaces/SearchPlantsActivity.java
@@ -2,6 +2,7 @@ package ws.tilda.anastasia.plantplacesapp.plantplaces;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import ws.tilda.anastasia.plantplacesapp.R;
 
@@ -11,5 +12,9 @@ public class SearchPlantsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_plants);
+    }
+
+    public void searchPlants(View v) {
+
     }
 }
