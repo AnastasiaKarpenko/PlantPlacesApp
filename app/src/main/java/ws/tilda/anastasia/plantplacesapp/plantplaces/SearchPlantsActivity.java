9fc470d9e66d990ae84e1feb5ea0eb3d4f18828b
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-21 14:42:25 +0300

    Wiring activity to the UI

--- app/src/main/java/ws/tilda/anastasia/plantplacesapp/plantplaces/SearchPlantsActivity.java
+++ app/src/main/java/ws/tilda/anastasia/plantplacesapp/plantplaces/SearchPlantsActivity.java
@@ -1,20 +1,30 @@
 package ws.tilda.anastasia.plantplacesapp.plantplaces;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 
 import ws.tilda.anastasia.plantplacesapp.R;
+import ws.tilda.anastasia.plantplacesapp.service.IPlantService;
+import ws.tilda.anastasia.plantplacesapp.service.PlantService;
 
 public class SearchPlantsActivity extends AppCompatActivity {
+
+    IPlantService plantService;
+    private AutoCompleteTextView actPlantName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_plants);
+        plantService = new PlantService();
+
+        actPlantName = findViewById(R.id.actPlantName);
+
     }
 
     public void searchPlants(View v) {
-
+        plantService.fetchPlants(actPlantName.getText().toString());
     }
 }
