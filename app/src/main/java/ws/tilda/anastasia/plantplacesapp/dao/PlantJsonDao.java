940c8eed16cbe0fb2b2a2a8292a9e6f1fe969bef
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-21 13:38:02 +0300

    Created an emplimentation of an interface

---
+++ app/src/main/java/ws/tilda/anastasia/plantplacesapp/dao/PlantJsonDao.java
@@ -0,0 +1,12 @@
+package ws.tilda.anastasia.plantplacesapp.dao;
+
+import java.util.List;
+
+import ws.tilda.anastasia.plantplacesapp.dto.PlantDTO;
+
+public class PlantJsonDao implements IPlantDAO {
+    @Override
+    public List<PlantDTO> fetchPlants(String filter) {
+        return null;
+    }
+}
