d243110fae9204d320310ef626e619ebb51746e6
Anastasia Karpenko <morganaloca@gmail.com>
2018-10-23 17:23:26 +0300

    Created test methods for the yet not created class.

--- app/src/test/java/ws/tilda/anastasia/plantplacesapp/TestPlantGPSDAO.java
+++ app/src/test/java/ws/tilda/anastasia/plantplacesapp/TestPlantGPSDAO.java
@@ -4,28 +4,36 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 public class TestPlantGPSDAO {
 
-    private ISpecimenDAO specimenDAO;
-    private List<SpecimenDTO> specimens;
-
-    @Test
-    public void testGPSDAO_fetchGPSReturnsResultsWithinRange() {
-        givenGPSDAOInitialized();
-        whenSearchForSpecimensNearBotanicGarden();
-        thenReturnSpecimensWithinRange();
-    }
-
-    private void thenReturnSpecimensWithinRange() {
-
-    }
-
-    private void whenSearchForSpecimensNearBotanicGarden() {
-
-    }
-
-    private void givenGPSDAOInitialized() {
-
-    }
+//    private ISpecimenDAO specimenDAO;
+//    private List<SpecimenDTO> specimens;
+//
+//    @Test
+//    public void testGPSDAO_fetchGPSReturnsResultsWithinRange() {
+//        givenGPSDAOInitialized();
+//        whenSearchForSpecimensNearBotanicGarden();
+//        thenReturnSpecimensWithinRange();
+//    }
+//
+//    private void thenReturnSpecimensWithinRange() {
+//        for (SpecimenDTO specimen : specimens) {
+//            double delta = 0.2;
+//            assertEquals(39.1461, specimen.getLatitude(), delta);
+//            assertEquals(-84.50826, specimen.getLongitude(), delta);
+//        }
+//
+//    }
+//
+//    private void whenSearchForSpecimensNearBotanicGarden() {
+//        specimens = specimenDAO.fetchSpecimens(39.1461, -84.50826);
+//    }
+//
+//    private void givenGPSDAOInitialized() {
+//        specimenDAO = new SpecimenDAO();
+//
+//    }
 
 }
